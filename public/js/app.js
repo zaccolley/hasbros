98c2d477a244e2d174d2edd35278454ba61e68c9
Zac Colley <hi@zaccolley.com>
2015-06-25 17:43:48 +0100

    Merged all the shit

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,6 @@ window.addEventListener('load', function () {
     var socket = io();
 
     var msgs = document.getElementById('msgs');
-    var task = document.getElementById('task');
     var status = document.getElementById('status');
     var leaderboards = document.getElementById('leaderboards');
     var name = document.getElementById('name');
@@ -21,24 +20,62 @@ window.addEventListener('load', function () {
 
     socket.on('success', function (msg) {
         msg = JSON.parse(msg);
-        task.innerHTML = "";
-        timer.innerHTML = "";
+
+        removeAction();
     });
 
     socket.on('event', function (msg) {
         msg = JSON.parse(msg);
 
-        task.innerHTML = msg.task;
-        timer.innerHTML = msg.timer;
+        playSound(msg.task);
+        changeAction(msg.task);
     });
 
     socket.on('fail', function (msg) {
         msg = JSON.parse(msg);
 
-        failed.innerHTML = msg.msg;
+        removeAction();
+        failedAction();
 
         start.removeAttribute('disabled');
 
         leaderboards.innerHTML += '<tr><td>' + msg.player + '</td><td>' + msg.score + '</td></tr>';
     });
+
 });
+
+// sounds
+
+function playSound(action){
+    var sfx = document.createElement('audio');
+    sfx.setAttribute('src', 'resources/'+action+'.mp3');
+    sfx.setAttribute('type', 'audio/mp3');
+    sfx.volume = 1;
+    sfx.play();
+}
+
+// actions
+
+function failedAction(){
+
+    $('.action').removeClass().addClass('action action--failed');
+    $('.action').text('Failed!');
+
+}
+
+function removeAction(){
+
+    $('.action').removeClass().addClass('action');
+    $('.action').text('');
+
+}
+
+function changeAction(action){
+
+    $('.action').removeClass().addClass('action action--fade action--'+action);
+    $('.action').text(action);
+    setTimeout(function(){
+        $('.action').removeClass('action--fade');
+    }, 3000);
+
+}
